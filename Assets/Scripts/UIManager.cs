29576a166d70478dde348ea0eaf503eebe636af2
Ray <jr071100@gmail.com>
2023-12-27 10:25:13 +0700

    Finished project

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+
+public class UIManager : MonoBehaviour
+{
+	//Метод удаления кнопок
+	public void RemoveButton(GameObject buttonToRemove)
+	{
+		Destroy(buttonToRemove);
+	}
+
+	//Метод закрытия окна
+	public void CloseWindow(GameObject panel)
+	{
+		panel.SetActive(false);
+	}
+
+	//Метод открытия окна
+	public void OpenWindow(GameObject panel)
+	{
+		panel.SetActive(true);
+	}
+}
